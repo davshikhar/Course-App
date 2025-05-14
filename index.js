b3d6c65221f13f1a7025a15775ca469c029fe6c0
Aditya <adityaranjan5456@gmail.com>
2025-05-14 17:46:15 +0530

    Commented a better way for db connection in index.js file

--- index.js
+++ index.js
@@ -12,5 +12,14 @@ app.use("/api/v1/user",userRouter);
 app.use("/api/v1/admine",adminRouter);
 app.use("/api/v1/course",courseRouter);
 
+//this is a better way since it makes sure that if the database is not connected then the backend does not even start.
+/*
+async function main(){
+    await mongoose.connect("mongodb+srv://admin:Mongodb123@cluster0.xuubqyp.mongodb.net/course-app");
+    app.listen(3000);
+}
+
+main();
+*/
 
 app.listen(3000);
